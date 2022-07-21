de028ecb3e974772f33984e5f7c3ffb50f0ad503
Miles MJ Jamon <pvzzombs@gmail.com>
2022-07-21 16:21:03 +0800

    Modified parser.cpp

--- parser.cpp
+++ parser.cpp
@@ -1,6 +1,8 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
 
 enum p_type{
+  LETTER
   WORD,
   SPACE,
   EOL,
@@ -11,13 +13,42 @@ enum p_token{
   p_type type;
   int col;
   int line;
+  char value;
+}
+
+bool is_alpha(char c){
+  return (c >= 'a' && c <= 'z') || 
+         (c >= 'A' && c <= 'Z');
+}
+
+bool is_space(char c){
+  return c == ' ' || c == '\t';
 }
 
 class parser{
 public:
-  void parse(std::string inp){
+  std::vector<p_token> parse(std::string inp){
+    std::vector<p_token> arr;
+    arr.reserve(inp.size());
+    p_token t;
+    int line = 1;
+
     for(int i=0; i<inp.size(); i++){
-      
+      if(is_alpha(inp.at(i))){
+        t.line = line;
+        t.col = i+1;
+        t.value = inp.at(i);
+        t.type = LETTER;
+        arr.push_back(t);
+      }else if(is_space(inp.at(i))){
+        t.line = line;
+        t.col = i+1;
+        t.value = inp.at(i);
+        t.type = SPACE;
+        arr.push_back(t);
+      }else if(is_eol(inp.at(i))){
+
+      }
     }
   }
 }
