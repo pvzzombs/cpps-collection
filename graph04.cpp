33fc084965023c7768969edf289a62a4c74b8725
Miles MJ Jamon <pvzzombs@gmail.com>
2023-01-09 17:28:24 +0800

    Refactored primMST()

---
+++ graph04.cpp
@@ -0,0 +1,252 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+//#include <cassert>
+#define MAX_VERTEX_COST 987654321
+
+class Edge{
+  public:
+  int dest;
+  int weight;
+  bool operator==(const Edge& rhs) const {
+    return dest == rhs.dest;
+  }
+  bool operator<(Edge const &rhs) const {
+    return dest < rhs.dest;
+  }
+};
+
+class Vertex{
+  public:
+  std::string name;
+  std::vector<Edge> edges;
+};
+
+class EdgeNode{
+  public:
+  int src;
+  int dest;
+  int weight;
+  bool operator<(const EdgeNode& rhs) const {
+    return weight < rhs.weight;
+  }
+};
+
+class UndirectedGraph{
+  public:
+  std::vector<Vertex> vertices;
+  int id=0;
+  void add_vertex(std::string name){
+    vertices.push_back(Vertex());
+    vertices.at(id).name = name;
+    std::cout << "Vertex " << id << " added successfully" << std::endl;
+    ++id;
+  }
+  void add_edge(int src, int dest, int weight=0){
+    Edge temp;
+    temp.dest = dest;
+    temp.weight = weight;
+    
+    auto it = std::find(vertices.at(src).edges.begin(), vertices.at(src).edges.end(), temp);
+    if(it == vertices.at(src).edges.end()){
+      vertices.at(src).edges.push_back(temp);
+    }
+    
+    temp.dest = src;
+
+    it = std::find(vertices.at(dest).edges.begin(), vertices.at(dest).edges.end(), temp);
+    if(it == vertices.at(dest).edges.end()){
+      vertices.at(dest).edges.push_back(temp);
+    }
+  }
+  void print(){
+    for(int i=0; i<vertices.size(); i++){
+      std::cout << vertices.at(i).name << " = { ";
+      for(int j=0; j<vertices.at(i).edges.size(); j++){
+        std::cout << "[" << vertices.at(i).edges.at(j).dest << ", " << vertices.at(i).edges.at(j).weight << "] ";
+      }
+      std::cout << "}" << std::endl;
+    }
+  }
+  void sort(){
+    for(int i=0; i<vertices.size(); i++){
+      std::sort(vertices.at(i).edges.begin(), vertices.at(i).edges.end());
+    }
+  }
+  int extract_min(std::vector<int> &key, std::vector<bool> &visited){
+    int index = -1;
+    int current_min = MAX_VERTEX_COST;
+    
+    for(int i=0; i<vertices.size(); i++){
+      if(!visited.at(i) && key.at(i) < current_min){
+        current_min = key.at(i);
+        index = i;
+      }
+    }
+    return index;
+  }
+  void primMST(int start=0){
+    std::vector<int> parent(vertices.size());
+    std::vector<int> key(vertices.size());
+    std::vector<bool> visited(vertices.size());
+    for(int i=0; i<visited.size(); i++){
+      parent.at(i) = -1;
+      visited.at(i) = false;
+      key.at(i) = MAX_VERTEX_COST;
+    }
+    
+    key.at(start) = 0;
+    
+    while(true){
+      int u = extract_min(key, visited);
+      if(u == -1){ break; }
+      visited.at(u) = true;
+      for(int i=0; i<vertices.at(u).edges.size(); i++){
+        int v = vertices.at(u).edges.at(i).dest;
+        int w = vertices.at(u).edges.at(i).weight;
+        if(!visited.at(v) && w < key.at(v)){
+          parent.at(v) = u;
+          key.at(v) = w;
+        }
+      }
+    }
+    
+    for(int i=0; i<parent.size(); i++){
+      if(i != start){
+        std::cout << i << " -> " << parent.at(i) << std::endl;
+      }
+    }
+  }
+  void hasCycle(){
+    std::vector<int> q;
+    std::vector<int> visited;
+    
+    q.push_back(0);
+    
+    while(q.size()){
+      auto e = q.front();
+      for(int i=0; i<vertices.at(e).edges.size(); i++){
+        auto d = vertices.at(e).edges.at(i).dest;
+        if(std::find(visited.begin(), visited.end(), d) == visited.end()){
+          if(std::find(q.begin(), q.end(), d) == q.end()){
+            q.push_back(d);
+          }else{
+            std::cout << "Has a cycle" << std::endl;
+            return;
+          }
+        }
+      }
+      
+      visited.push_back(e);
+      q.erase(q.begin());
+    }
+    
+    std::cout << "Has no cycle" << std::endl;
+  }
+  void BFS(int v=0){
+    std::vector<int> q;
+    std::vector<int> visited;
+    
+    q.push_back(v);
+    
+    while(q.size()){
+      auto e = q.front();
+      for(int i=0; i<vertices.at(e).edges.size(); i++){
+        auto d = vertices.at(e).edges.at(i).dest;
+        if(std::find(visited.begin(), visited.end(), d) == visited.end() && std::find(q.begin(), q.end(), d) == q.end()){
+          q.push_back(d);
+        }
+      }
+      
+      visited.push_back(e);
+      q.erase(q.begin());
+    }
+    
+    for(int i=0; i<visited.size(); i++){
+      std::cout << visited.at(i);
+      if(i+1<visited.size()){
+        std::cout << ", ";
+      }
+    }
+    std::cout << std::endl;
+  }
+  void DFSBT(std::vector<int> &s, std::vector<int> &visited){
+    for(int i=0; i<vertices.at(s.back()).edges.size(); i++){
+      auto e = vertices.at(s.back()).edges.at(i).dest;
+      if(std::find(visited.begin(), visited.end(), e) == visited.end()){
+        s.push_back(e);
+        visited.push_back(e);
+        DFSBT(s, visited);
+        s.pop_back();
+      }
+    }
+  }
+  void DFS(int v=0){
+    std::vector<int> s;
+    std::vector<int> visited;
+    
+    s.push_back(v);
+    visited.push_back(v);
+    DFSBT(s, visited);
+    s.pop_back();
+    
+    for(int i=0; i<visited.size(); i++){
+      std::cout << visited.at(i);
+      if(i+1<visited.size()){
+        std::cout << ", ";
+      }
+    }
+    std::cout << std::endl;
+  }
+};
+
+int main(){
+  UndirectedGraph a;
+  a.add_vertex("0");
+  a.add_vertex("1");
+  a.add_vertex("2");
+  a.add_vertex("3");
+  a.add_vertex("4");
+  
+  /*a.add_vertex("5");
+  a.add_vertex("6");*/
+  
+  a.add_edge(0, 1, 1);
+  a.add_edge(1, 4, 2);
+  a.add_edge(0, 2, 4);
+  a.add_edge(2, 4, 3); // Replace 3 with 0
+  a.add_edge(0, 3, 10);
+  a.add_edge(3, 4, 11);
+  /*a.add_edge(0,1,2);
+  a.add_edge(0,2,1);
+  a.add_edge(0,4,3);
+  a.add_edge(1,3,1);
+  a.add_edge(2,3,1);
+  a.add_edge(4,3,3);*/
+  /*a.add_edge(0, 1);
+  a.add_edge(1, 2);
+  a.add_edge(1, 4);
+  a.add_edge(2, 3);
+  a.add_edge(4, 3);*/
+  
+  /*a.add_edge(0,1);
+  a.add_edge(0,2);
+  a.add_edge(1,3);
+  a.add_edge(1,4);
+  a.add_edge(2,5);
+  a.add_edge(2,6);*/
+  /*a.add_edge(0,1);
+  a.add_edge(1,2);
+  a.add_edge(0,3);
+  a.add_edge(0,4);
+  a.add_edge(1,5);
+  a.add_edge(1,6);*/
+  
+  a.sort();
+  a.print();
+  a.primMST();
+  a.hasCycle();
+  a.BFS();
+  a.DFS();
+  return 0;
+}
